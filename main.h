18a12a7f7416b04d0bd344ca28cb1ba31598d06c
Akacha Ubuntu <samchijioke16@gmail.com>
2022-07-30 17:53:35 +0100

    First main.h push

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void prompt_cmd(void);
+char **read_cmd(void);
+char **parse_cmd(char *str);
+int _strlen(char *s);
